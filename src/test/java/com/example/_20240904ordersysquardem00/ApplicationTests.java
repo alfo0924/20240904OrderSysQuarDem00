21969ee8a444ce300eed4a0789ed6f672c4b8806
alfo0924 <halo0924@yahoo.com.tw>
2024-09-04 20:31:10 +0800

    v1.01 cnp

---
+++ src/test/java/com/example/_20240904ordersysquardem00/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._20240904ordersysquardem00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
