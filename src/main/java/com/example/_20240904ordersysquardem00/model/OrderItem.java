e80c761834dea64ec77e4ee85f4d5f825c7e1fe8
alfo0924 <halo0924@yahoo.com.tw>
2024-09-12 22:28:30 +0800

    v1.1075 fix order-items api issue  cnp

--- src/main/java/com/example/_20240904ordersysquardem00/model/OrderItem.java
+++ src/main/java/com/example/_20240904ordersysquardem00/model/OrderItem.java
@@ -1,5 +1,6 @@
 package com.example._20240904ordersysquardem00.model;
 
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,19 +9,52 @@ import lombok.Setter;
 @Table(name = "order_items")
 @Getter
 @Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "orderItemsId")
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String orderItemsId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "items_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private FoodItem foodItem;
 
     private Integer counts;
     private Double amount;
+
+    @JsonIgnore
+    public Order getOrder() {
+        return order;
+    }
+
+    @JsonProperty
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @JsonIgnore
+    public FoodItem getFoodItem() {
+        return foodItem;
+    }
+
+    @JsonProperty
+    public void setFoodItem(FoodItem foodItem) {
+        this.foodItem = foodItem;
+    }
+
+    @JsonProperty("orderId")
+    public String getOrderId() {
+        return order != null ? order.getOrderId() : null;
+    }
+
+    @JsonProperty("foodItemId")
+    public String getFoodItemId() {
+        return foodItem != null ? foodItem.getItemsId() : null;
+    }
 }
