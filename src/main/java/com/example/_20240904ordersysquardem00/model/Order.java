5028e05a8e5004a7181f5c5a8b9fea54460c2fc5
alfo0924 <halo0924@yahoo.com.tw>
2024-09-12 22:15:33 +0800

    v1.107 fix order issue  cnp

--- src/main/java/com/example/_20240904ordersysquardem00/model/Order.java
+++ src/main/java/com/example/_20240904ordersysquardem00/model/Order.java
@@ -1,5 +1,6 @@
 package com.example._20240904ordersysquardem00.model;
 
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,26 +12,78 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "orderId")
 public class Order {
     @Id
     private String orderId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private Customer customer;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private Restaurant restaurant;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private DeliveryStaff deliveryStaff;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date orderDate;
+
     private Double orderPrice;
     private String orderStatus;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<OrderItem> orderItems;
+
+    @JsonIgnore
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    @JsonProperty
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @JsonIgnore
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    @JsonProperty
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @JsonIgnore
+    public DeliveryStaff getDeliveryStaff() {
+        return deliveryStaff;
+    }
+
+    @JsonProperty
+    public void setDeliveryStaff(DeliveryStaff deliveryStaff) {
+        this.deliveryStaff = deliveryStaff;
+    }
+
+    @JsonProperty("customerId")
+    public Long getCustomerId() {
+        return customer != null ? customer.getCustomerId() : null;
+    }
+
+    @JsonProperty("restaurantId")
+    public Long getRestaurantId() {
+        return restaurant != null ? restaurant.getRestaurantId() : null;
+    }
+
+    @JsonProperty("deliveryStaffId")
+    public String getDeliveryStaffId() {
+        return deliveryStaff != null ? deliveryStaff.getDeliveryId() : null;
+    }
 }
