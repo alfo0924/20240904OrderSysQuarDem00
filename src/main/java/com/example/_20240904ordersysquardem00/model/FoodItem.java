2446324977fd4fe3ce76161749f52de9bffac116
alfo0924 <halo0924@yahoo.com.tw>
2024-09-12 23:07:26 +0800

    v1.108 fix foodItem.java api issue  cnp

--- src/main/java/com/example/_20240904ordersysquardem00/model/FoodItem.java
+++ src/main/java/com/example/_20240904ordersysquardem00/model/FoodItem.java
@@ -1,5 +1,6 @@
 package com.example._20240904ordersysquardem00.model;
 
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,15 +9,32 @@ import lombok.Setter;
 @Table(name = "food_items")
 @Getter
 @Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "itemsId")
 public class FoodItem {
     @Id
     private String itemsId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "menu_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private Menu menu;
 
     private String foodName;
     private Double foodPrice;
     private String foodStatus;
+
+    @JsonIgnore
+    public Menu getMenu() {
+        return menu;
+    }
+
+    @JsonProperty
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    @JsonProperty("menuId")
+    public String getMenuId() {
+        return menu != null ? menu.getMenuId() : null;
+    }
 }
